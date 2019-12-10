db061b171a51ff4464c1cff1a1a3d85e1553065d
Maxim <maksfomin@gmail.com>
2019-12-10 18:29:23 +0300

    Start Project

---
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -0,0 +1,36 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.2f, 0.6f, 0.5f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.setColor(0.3f, 0.5f, 0.6f, 1f);
+		batch.draw(img, 10, 10,400, 400);
+		batch.setColor(0.3f, 0.5f, 0.6f, 0.5f);
+		batch.draw(img, 300, 300,400, 400);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
