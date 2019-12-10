6d877ebd324a9a1292ca7ec985277653530bba70
Maxim <maksfomin@gmail.com>
2019-12-10 19:58:04 +0300

    Start create StarGame+

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -4,18 +4,21 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture background;
+	private SpriteBatch batch;
+    private Sprite backgroundSprite;
+	private Texture img;
+	private Texture background;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		background = new Texture("space.jpg");
+        backgroundSprite =new Sprite(background);
 	}
 
 	@Override
@@ -23,7 +26,8 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.2f, 0.6f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(background, 0, 0);
+        renderBackground();
+		// batch.draw(background, 0, 0);
 		batch.end();
 	}
 
@@ -32,4 +36,9 @@ public class StarGame extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+    public void renderBackground() {
+        backgroundSprite.draw(batch);
+    }
+
 }
