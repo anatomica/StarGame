b60db4e60c0f8db16332100a07434da553aba082
anatomica <maksfomin@gmail.com>
2019-12-18 00:42:53 +0300

    Create HW 2

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -10,29 +10,39 @@ public class MenuScreen extends BaseScreen {
 
     private Texture img;
     private Texture background;
-    private Vector2 pos;
-    private Vector2 v;
+    private Vector2 position;
+    private Vector2 endPoint;
+    private Vector2 vector;
+    private Vector2 between;
+    private float speed = 2;
 
     @Override
     public void show() {
         super.show();
-        background = new Texture("textures/bg.png");
+        background = new Texture("space.jpg");
         img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-        v = new Vector2(0.5f, 0.5f);
+        vector = new Vector2();
+        position = new Vector2();
+        between = new Vector2();
+        endPoint = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(0.2f, 	0.6f, 0.5f, 1);
+        Gdx.gl.glClearColor(0.2f,     0.6f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
         batch.draw(background, 0, 0);
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, position.x, position.y);
         batch.end();
-        pos.add(v);
+
+        between = endPoint.cpy().sub(position);
+        if (between.len() > vector.len()){
+            // System.out.println("between.len() - " + between.len() + " vector.len() - " + vector.len());
+            position.add(vector);
+        } else position = endPoint;
     }
 
     @Override
@@ -44,7 +54,11 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
+        endPoint = new Vector2(screenX,Gdx.graphics.getHeight() - screenY);
+//        делаем копию конечной точки, вычитаем из нее координаты начальной точки,
+//        нормализуем, ускоряем сколяром и устанавливаем это в вектор направления
+        vector.set(endPoint.cpy().sub(position).nor().scl(speed));
         return false;
     }
+
 }
